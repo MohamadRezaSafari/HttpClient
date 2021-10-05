d7c069dc17f954a907fcef29f8a67fd1dd4f8e3a
Mohamad Reza Safari <mrdev1020@gmail.com>
2021-10-05 10:26:58 +0330

    add api

---
+++ Movies/Client/Movies.Client/Services/CRUDService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Services
+{
+    public class CRUDService : IIntegrationService
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public CRUDService()
+        {
+            // set up HttpClient instance
+            httpClient.BaseAddress = new Uri("http://localhost:57863");
+            httpClient.Timeout = new TimeSpan(0, 0, 30);
+        }
+        
+        public async Task Run()
+        {
+            await GetResource();
+        }
+
+        public async Task GetResource()
+        {
+            var response = await httpClient.GetAsync("api/movies");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
